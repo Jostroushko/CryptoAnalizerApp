45946a747a0083cb004a78d77ad481703845d979
Ostroushko Julia <jostroushko@gmail.com>
2024-09-23 13:41:48 +0700

    реализован валидатор, подправлен BruteForce

--- CryptoAnalyzer/src/main/java/org/example/BruteForce.java
+++ CryptoAnalyzer/src/main/java/org/example/BruteForce.java
@@ -10,9 +10,9 @@ public class BruteForce {
     public String decryptByBruteForce(String encryptedText, char[] alphabet) {
         int alphabetLength = alphabet.length;
         // перебор сдвигов
-        for (int shift = 0; shift < alphabetLength; shift++) {
-            String decrypt = new Cipher().decrypt(encryptedText, alphabet[shift]);
-             if (isDecryptedCorrectly(decrypt)) {
+        for (char c : alphabet) {
+            String decrypt = new Cipher().decrypt(encryptedText, c);
+            if (isDecryptedCorrectly(decrypt)) {
                 return decrypt;
             }
         }
--- CryptoAnalyzer/src/main/java/org/example/Validator.java
+++ CryptoAnalyzer/src/main/java/org/example/Validator.java
@@ -1,12 +1,18 @@
 package org.example;
 
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Validator {
     public boolean isValidKey(int key, char[] alphabet) {
         // Проверка ключа
-        return false;
+        return key >= 0 && key < alphabet.length;
     }
     public boolean isFileExists(String filePath) {
         // Проверка существования файла
-        return false;
+        Path path = Paths.get(filePath);
+        return Files.exists(path) && Files.isRegularFile(path);
     }
 }
