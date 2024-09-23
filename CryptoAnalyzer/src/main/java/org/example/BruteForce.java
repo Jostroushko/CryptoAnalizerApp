e49f1a7c2783535c6810376274a40d7988552f1d
Ostroushko Julia <jostroushko@gmail.com>
2024-09-23 11:55:15 +0700

    реализована часть BruteForce с перебором возможных сдвигов

--- CryptoAnalyzer/src/main/java/org/example/BruteForce.java
+++ CryptoAnalyzer/src/main/java/org/example/BruteForce.java
@@ -2,7 +2,14 @@ package org.example;
 
 public class BruteForce {
     public String decryptByBruteForce(String encryptedText, char[] alphabet) {
-        // Логика brute force
-        return "";
+        int alphabetLength = alphabet.length;
+        // перебор сдвигов
+        for (int shift = 0; shift < alphabetLength; shift++) {
+            String decrypt = new Cipher().decrypt(encryptedText, alphabet[shift]);
+            // if () {
+                return decrypt;
+           // }
+        }
+        return "Расшифровку выполнить невозможно";
     }
 }
