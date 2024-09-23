3decf14c191a9684602f23278347cd688ba4ca70
Ostroushko Julia <jostroushko@gmail.com>
2024-09-23 11:16:36 +0700

    реализована логика шифрования и расшифровки

--- CryptoAnalyzer/src/main/java/org/example/Cipher.java
+++ CryptoAnalyzer/src/main/java/org/example/Cipher.java
@@ -1,16 +1,35 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Cipher {
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
 
     public String encrypt(String text, int shift) {
-        // Логика шифрования
-        return "";
+        StringBuilder result = new StringBuilder();
+        // пусть сдвиг ограничивает размер алфавита, чтобы не уйти за пределы
+        shift = shift % ALPHABET.length;
+        for (char c : text.toCharArray()) {
+            // toLowerCase, ведь прописные буквы - другой символ, упростим поиск
+            int index = Arrays.binarySearch(ALPHABET, Character.toLowerCase(c));
+            if (index >= 0) {
+                // рассчитываем новый индекс, взяв старый и сдвиг с остатком от деления на размер алфавита
+                int newIndex = (index + shift) % ALPHABET.length;
+                // символ найден
+                result.append(ALPHABET[newIndex]);
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
     }
     public String decrypt(String encryptedText, int shift) {
-        // Логика расшифровки
-        return "";
+        // Логика расшифровки состоит в том, чтобы выполнить сдвиг в обратном направлении
+        // также, ограничим сдвиг алфавитом
+        shift = shift % ALPHABET.length;
+        // используем функцию, которая уже есть
+        return encrypt(encryptedText, -shift);
     }
 }
