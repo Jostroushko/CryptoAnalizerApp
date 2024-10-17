7bb90ca90a5a733b48cadb0fb9b2477efdbbe7c3
Ostroushko Julia <jostroushko@gmail.com>
2024-10-17 11:08:25 +0700

    корректировка брут форс, корректировка кнопки, учет выхода за пределы диапазона, валидация

--- CryptoAnalyzer/src/main/java/org/example/Cipher.java
+++ CryptoAnalyzer/src/main/java/org/example/Cipher.java
@@ -3,12 +3,16 @@ package org.example;
 import java.util.Arrays;
 
 public class Cipher {
+    static final Validator validator = new Validator();
     static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
 
     public String encrypt(String text, int shift) {
         StringBuilder result = new StringBuilder();
+
+        if (!validator.isValidKey(shift, ALPHABET)) throw new IllegalArgumentException("Invalid key");
+
         shift = shift % ALPHABET.length;
 
         for (char c : text.toCharArray()) {
--- CryptoAnalyzer/src/main/java/org/example/FileManager.java
+++ CryptoAnalyzer/src/main/java/org/example/FileManager.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,13 +7,20 @@ import java.nio.file.Paths;
 import java.nio.charset.StandardCharsets;
 
 public class FileManager {
+    static final Validator validator = new Validator();
     public String readFile(String filePath) throws IOException {
+        if (!validator.isFileExists(filePath)) {
+            throw new FileNotFoundException(filePath);
+        }
         Path path = Paths.get(filePath);
         byte[] bytes = Files.readAllBytes(path);
         return new String(bytes, StandardCharsets.UTF_8);
     }
 
     public void writeFile(String content, String filePath) throws IOException {
+        if (!validator.isFileExists(filePath)) {
+            throw new FileNotFoundException(filePath);
+        }
         Path path = Paths.get(filePath);
         Files.writeString(path, content);
     }
